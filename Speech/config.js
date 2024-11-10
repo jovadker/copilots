8cbf5916bdb9965dc0d059656ca0c6a18aa4c72d
József Vadkerti <jovadker@microsoft.com>
2024-11-10 19:09:32 +0100

    added speech to the webchat

---
+++ Speech/config.js
@@ -0,0 +1,6 @@
+const config = {
+    directLineTokenUrl: 'REPLACE WITH YOUR DIRECTLINE ENDPOINT',
+    directLineDomain: 'https://europe.directline.botframework.com/v3/directline', // for Europe 
+    secret: 'YOUR SECRET',
+    speechServiceApiKey: 'YOUR SPEECH SERVICE API KEY'
+  };
