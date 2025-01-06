a8da9a09fb3da62aaa67fea2308c01870eae9128
József Vadkerti <jovadker@microsoft.com>
2025-01-06 18:33:22 +0100

    updated deployment scripts for iac

--- acs/deploy.sh
+++ acs/deploy.sh
@@ -4,10 +4,12 @@ set -e
 # az login (MS FDPO tenant)
 # az account set --subscription baa70448-593c-4dc7-8a91-c92cf7eaf66e
 
-current_date_time=$(date +%Y%m%d%H%M%S)
+current_date_time=$(date +%Y%m%d)
 
 resource_group_name=Telephony.Automated.RG
 location=eastus
+botId=copilotstudiobot$current_date_time
+web_app_name=automatedbot$current_date_time
 
 az group create --name $resource_group_name --location $location
 
@@ -17,8 +19,6 @@ tenant_id=$(az account show --query tenantId --output tsv)
 identity_name=automatedbotIdentity
 identity=$(az identity create --resource-group $resource_group_name --name $identity_name --location $location)
 
-echo "output:" + $identity
-
 identity_id=$(echo $identity | jq -r '.id')
 client_id=$(echo $identity | jq -r '.clientId')
 resource_id=$(echo $identity | jq -r '.id')
@@ -27,28 +27,31 @@ echo "Client ID: $client_id"
 echo "Resource ID: $resource_id"
 echo "Tenant ID: $tenant_id"
 
-botId=copilotstudiobot$current_date_time
+
 
 az deployment group create --resource-group $resource_group_name \
     --template-file ./iac/BotWebApp.json \
-    --parameters appServiceName=automatedbot \
+    --parameters appServiceName=$web_app_name \
       appServicePlanName=automatedbotAppPlan appServicePlanLocation=$location \
-      appType=UserAssignedMSI appId=$identity_id appSecret="" tenantId=$tenant_id \
+      appType=UserAssignedMSI appId=$client_id appSecret="" tenantId=$tenant_id \
       UMSIName=$identity_name UMSIResourceGroupName=$resource_group_name \
     --verbose
-
-
-# app id = client id of managed identity
-# tenant id = tenant id of the subscription
-# msiResourceId = /subscriptions/baa70448-593c-4dc7-8a91-c92cf7eaf66e/resourceGroups/Telephony.Automated.RG/providers/Microsoft.ManagedIdentity/userAssignedIdentities/automatedbotIdentity
 
 az deployment group create --resource-group $resource_group_name \
     --template-file ./iac/bot.json \
     --parameters appType=UserAssignedMSI botId=$botId \
-      msAppId=$client_id tenantId=$tenant_id msiResourceId=$identity_id
+      msAppId=$client_id tenantId=$tenant_id msiResourceId=$identity_id \
+      messagingEndpoint=https://$web_app_name.azurewebsites.net/api/messages \
 
 
 # building the app
-dotnet build ./src/jovadkerecho/EchoBot.csproj
+#dotnet build ./src/jovadkerecho/EchoBot.csproj -c Release
+#dotnet publish ./src/jovadkerecho/EchoBot.csproj -c Release -o ./publish
+# az webapp deploy --resource-group Telephony.Automated.RG --name automatedbot20250106 --src-path ./publish.zip --type zip
 
+#dotnet publish ./src/jovadkerecho/EchoBot.csproj -c Release -p:WebPublishMethod=Package -p:PackageLocation=./publish.zip
+
+#tar -czf publish.tar.gz -C ./jovadkerecho .
+
+#az webapp deploy --resource-group $resource_group_name --name $web_app_name --src-path ./publish
 
